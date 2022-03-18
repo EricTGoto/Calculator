313ab619959ad3134c844363b9b5b3b0de092cd4
EricTGoto <erictomokigoto@gmail.com>
2022-03-18 16:54:28 -0700

    added operations and implemented their functionality

--- calculator.js
+++ calculator.js
@@ -1,10 +1,52 @@
-numberButtons = document.querySelectorAll('.number-buttons');
+let operations = []
+
+const numberButtons = document.querySelectorAll('.number-button');
 numberButtons.forEach(button => {
-    button.addEventListener("click", (e) => addNumberToDisplay(e))
     console.log(button)
+    button.addEventListener("click", (e) => updateDisplay(false, e))
 });
 
-function addNumberToDisplay(e) {
+const operationButtons = document.querySelectorAll('.operation-button');
+operationButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        display = document.querySelector('.display');
+        operations.push(display.textContent)
+        operations.push(e.target.textContent)
+        display.textContent = ""
+    })
+});
+
+const equalsButton = document.querySelector('.equals-button');
+equalsButton.addEventListener("click", operate)
+
+
+function updateDisplay(operation, e=undefined, result="" ) {
     display = document.querySelector('.display');
-    display.textContent = display.textContent + e.target.textContent;
+    if (!operation) {
+        display.textContent = display.textContent + e.target.textContent;
+    }
+    else {
+        display.textContent = result
+    }
+}
+    
+function operate() {
+    const display = document.querySelector('.display');
+    const operand2 = parseFloat(display.textContent);
+    let result = 0
+
+    const operator = operations.pop();
+    const operand1 = parseFloat(operations.pop());
+
+    if (operator == "+") {
+        result = operand1 + operand2;
+    } else if (operator == "-") {
+        result = operand1 - operand2;
+    } else if (operator == "/") {
+        result = operand1 / operand2;
+    } else {
+        result = operand1 * operand2;
+    }
+    operations.push(result);
+    updateDisplay(true, undefined, result)
 }
